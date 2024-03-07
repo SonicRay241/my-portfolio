5f534fc64c92c5448fdd53ec8dcb031bad8e63d4
SonicRay241 <rayhan.permana@yahoo.com>
2024-03-07 17:21:34 +0700

    should improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Rayhan Permana</title>
+    <meta name="title" content="Rayhan Permana" />
+    <meta name="description" content="A student in BINUS University who is also a Software Engineer." />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://rayy.dev" />
+    <meta property="og:title" content="Rayhan Permana" />
+    <meta property="og:description" content="A student in BINUS University who is also a Software Engineer." />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://rayy.dev" />
+    <meta property="twitter:title" content="Rayhan Permana" />
+    <meta property="twitter:description" content="A student in BINUS University who is also a Software Engineer." />
+
       <body className={font.className}>
         {children}
       </body>

ced1b84969797035738f160b2b81cfc0748d47f3
SonicRay241 <rayhan.permana@yahoo.com>
2024-03-12 22:29:49 +0700

    refactored about component

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -5,16 +5,6 @@ import { Property } from "csstype"
 import { Variants, motion, cubicBezier } from "framer-motion"
 import { useIsInViewport } from "@/libs/hooks"
 
-const Contacts: FC<{
-    
-}> = () => {
-    return (
-        <div className="w-28">
-
-        </div>
-    )
-}
-
 const About: FC<{ 
     mouseEnterHandler: (size: number, color: Property.BackgroundColor, blendMode?: Property.MixBlendMode) => void, 
     mouseLeaveHandler: () => void,
@@ -84,8 +74,8 @@ const About: FC<{
 
 
     return (
-        <div className="flex justify-center w-full bg-[#050505] pt-20 pb-28 px-6 drop-shadow-lg" ref={aboutRef} onMouseEnter={() => props.mouseEnterHandler(40, "white", "normal")}>
-            <div className="flex max-w-screen-xl lg:flex-row flex-col lg:gap-0 gap-11">
+        <div className="flex flex-col items-center w-full bg-[#050505] pt-20 px-6 drop-shadow-lg" ref={aboutRef} onMouseEnter={() => props.mouseEnterHandler(40, "white", "normal")}>
+            <div className="flex max-w-screen-xl lg:flex-row flex-col lg:gap-0 gap-11 pb-28">
             <div className="w-full lg:w-2/5 h-fit overflow-hidden">
                 <div 
                     className="flex flex-row text-7xl text-white w-fit lg:ml-8"
@@ -103,54 +93,18 @@ const About: FC<{
                         </motion.h1>)
                     }) }
                 </div>
-                {/* <motion.h1 
-                    className="text-7xl text-white w-fit lg:ml-8"
-                    variants={variants}
-                    animate="title"
-                    onMouseEnter={() => props.mouseEnterHandler(120, "white")} 
-                    onMouseLeave={() => props.mouseEnterHandler(40, "white", "normal")}
-                    >
-                    About
-                </motion.h1> */}
-                
-            
                 </div>
                 <div className="w-full lg:w-3/5">
                     <motion.p 
-                    className="text-white md:text-xl text-lg overflow-hidden"
+                    className="text-white md:text-2xl text-lg overflow-hidden"
                     variants={variants}
                     animate="paragraph"
                     onMouseEnter={() => props.mouseEnterHandler(240, "white")} 
                     onMouseLeave={() => props.mouseEnterHandler(40, "white", "normal")}>
-                        {/* I&apos;m a passionate web developer adept at crafting purpose-driven software solutions using diverse tools. 
-                        Currently, I balance studies with a role at <a href="http://ict.binus.edu/" className="text-red-500 hover:underline">BINUS IT Division</a>, 
-                        thriving on collaboration and continuous learning. */}
-                        {/* I&apos;m a dedicated web developer and a data science student, propelled by an unwavering passion for technology and innovation. 
-                        My journey into programming was ignited by an insatiable curiosity and a fervent desire to create solutions 
-                        that make a meaningful impact. With each project, I dive deep into the realm of versatile tools, harnessing 
-                        a diverse array of technologies to sculpt software solutions finely tuned to their intended purpose. 
-                        <br/><br/>Whether it&apos;s crafting intuitive user interfaces, architecting robust backend systems, or fine-tuning 
-                        performance, I thrive on the challenges and rewards that each endeavor brings. Currently, I&apos;m 
-                        diligently pursuing my academic aspirations while immersing myself in real-world experiences as a valued 
-                        member of <a href="http://ict.binus.edu/" target="_blank" className="text-violet-600 hover:underline">BINUS IT Division</a>. 
-                        Here, amidst dynamic projects and alongside talented peers, I contribute my expertise to push boundaries and 
-                        achieve collective goals. */}
                         I&apos;m a passionate web developer and data science student driven by curiosity and a commitment to impactful innovation. 
                         From crafting intuitive interfaces to architecting robust backend systems, I thrive on diverse technological challenges. 
                         Currently balancing academic pursuits with real-world experiences at <a href="http://ict.binus.edu/" target="_blank" className="text-violet-600 hover:underline">BINUS IT Division</a>, I collaborate with talented peers to push boundaries and achieve collective goals.
-                        {/* I&apos;m a */}
-                        {/* I&apos;m a dedicated web developer driven by a profound passion for technology and innovation.
-                        My journey into the realm of programming began with an insatiable curiosity and a desire to create impactful solutions.
-                        As a fervent believer in the power of versatile tools, I relish the opportunity to employ 
-                        a diverse array of technologies to craft software solutions tailored to their intended purpose.
-                        <br/><br/> Whether it's designing intuitive user interfaces, architecting robust backend systems,
-                        or optimizing performance, I thrive on the challenges and rewards that each project presents.
-                        Currently, I&apos;m pursuing my academic endeavors while also immersing myself in real-world 
-                        experiences as part of the <a href="http://ict.binus.edu/" className="text-red-500 hover:underline">BINUS IT Division</a>, 
-                        where I contribute to dynamic projects and collaborate with talented individuals to push boundaries 
-                    and achieve collective goals. */}
                     </motion.p>
-
                 </div>
             </div>
         </div>

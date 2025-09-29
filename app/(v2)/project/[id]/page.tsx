314d8636fcdd39439aa1ecaa3b49419e0ed8640f
SonicRay241 <rayhan.permana@yahoo.com>
2025-09-29 14:02:35 +0700

    Fixed pageprops issue

--- app/(v2)/project/[id]/page.tsx
+++ app/(v2)/project/[id]/page.tsx
@@ -1,10 +1,6 @@
 import ClientPage from "./_client"
 
-export default async function Page(props: {
-  params: {
-    id: string
-  }
-}) {
+export default async function Page(props: PageProps<'/project/[id]'>) {
   const { id } = await props.params
 
   return <ClientPage projectId={id}/>

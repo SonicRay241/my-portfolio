6393521acc86aad5dd16a5e5205f87a2ab46f4df
SonicRay241 <rayhan.permana@yahoo.com>
2025-12-25 13:59:10 +0700

    Updated footer

--- components/v2/footer.tsx
+++ components/v2/footer.tsx
@@ -2,16 +2,14 @@
 
 import CopyrightIcon from "@mui/icons-material/Copyright";
 import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
-import ScrollVelocity from "./scrollvelocity";
-import CurvedLoop from "./curvedmarquee";
 
 export default function Footer() {
   return (
     <div className="w-full mt-12 px-2 py-2">
       <div className="w-full p-4 text-white bg-black rounded-lg">
-      <p className="w-full text-xs text-zinc-500 mb-4">
-        Last updated September 2025
-      </p>
+        <p className="w-full text-xs text-zinc-500 mb-4">
+          Last updated December 2025
+        </p>
         <p className="text-zinc-500">Contact</p>
         <a
           href="mailto:rayhan.permana@outlook.com"

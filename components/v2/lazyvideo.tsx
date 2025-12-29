2935f33ba0a9896e6f7ea3179629d546e838921e
SonicRay241 <rayhan.permana@yahoo.com>
2025-12-29 14:11:46 +0700

    Fixed lazy video not loading on refresh

--- components/v2/lazyvideo.tsx
+++ components/v2/lazyvideo.tsx
@@ -19,8 +19,19 @@ export default function LazyVideo(props: VideoHTMLAttributes<HTMLVideoElement> &
   } = props
 
   const containerRef = useRef<HTMLSpanElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
   const [shouldLoad, setShouldLoad] = useState(initLoad || false);
   const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const video = videoRef.current
+    if (!video) return
+
+    // HAVE_METADATA or higher
+    if (video.readyState >= HTMLMediaElement.HAVE_METADATA) {
+      setIsLoaded(true)
+    }
+  }, [])
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -44,19 +55,19 @@ export default function LazyVideo(props: VideoHTMLAttributes<HTMLVideoElement> &
     <span ref={containerRef} className={`relative block ${className}`}>
       {/* Loading text */}
       {shouldLoad && !isLoaded && (
-        <span className="absolute inset-0 w-full p-6 flex items-center justify-center text-sm bg-zinc-800 text-zinc-500">
+        <span className="absolute inset-0 w-full p-6 flex items-center justify-center text-sm bg-zinc-800 text-zinc-500 xl:text-base 2xl:text-lg 3xl:text-xl 4xl:text-2xl">
           {loadingText || "Loading video..."}
         </span>
       )}
 
       {/* Video */}
       <video
+        ref={videoRef}
         src={shouldLoad ? src : undefined}
         className="h-auto w-full block"
         onCanPlay={() => setIsLoaded(true)}
         onLoadedMetadata={() => setIsLoaded(true)}
         preload={shouldLoad ? "auto" : "none"}
-        style={{ display: isLoaded ? "block" : "none" }}
         {...rest}
       />
     </span>

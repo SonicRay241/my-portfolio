18b1c99efe7b2f09d440ce36d63922db47f09697
SonicRay241 <rayhan.permana@yahoo.com>
2025-12-30 17:06:44 +0700

    Optimized noise animation

--- components/v2/FuzzyOverlay.tsx
+++ components/v2/FuzzyOverlay.tsx
@@ -1,27 +1,14 @@
 "use client"
 
-import { motion } from "motion/react"
 import blackNoise from "@/public/black-noise.png"
 
 export default function FuzzyOverlay() {
     return (
-        <motion.div
-            initial={{
-                transform: "translateX(-10%) translateY(-10%)"
-            }}
-            animate={{
-                transform: "translateX(10%) translateY(10%)",
-            }}
-            transition={{
-                repeat: Infinity,
-                duration: 0.2,
-                ease: "linear",
-                repeatType: "mirror",
-            }}
+        <div
             style={{
                 backgroundImage: `url(${blackNoise.src})`,
             }}
-            className="pointer-events-none fixed -inset-[100%] opacity-5"
+            className="pointer-events-none fixed -inset-[100%] opacity-5 animate-noise"
         />
     );
 };

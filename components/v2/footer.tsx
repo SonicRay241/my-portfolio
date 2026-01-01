177565ac62fa0b90cdcb4f12f88ce916b7c0912b
SonicRay241 <rayhan.permana@yahoo.com>
2026-01-01 20:45:36 +0700

    Added github to footer

--- components/v2/footer.tsx
+++ components/v2/footer.tsx
@@ -5,10 +5,10 @@ import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 
 export default function Footer() {
   return (
-    <div className="w-full mt-12 px-2 py-2">
+    <div className="w-full px-2 py-2">
       <div className="w-full p-4 text-white bg-black rounded-lg">
         <p className="w-full text-xs text-zinc-500 mb-4">
-          Last updated December 2025
+          Last updated January 2026
         </p>
         <p className="text-zinc-500">Contact</p>
         <a
@@ -36,6 +36,15 @@ export default function Footer() {
           rel="noreferrer"
         >
           <span>LinkedIn</span>
+          <ArrowOutwardIcon fontSize="small" className="" />
+        </a>
+        <a
+          href="https://github.com/SonicRay241"
+          className="text-lg flex items-center transition-colors hover:text-violet-600"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span>Github</span>
           <ArrowOutwardIcon fontSize="small" className="" />
         </a>
 
